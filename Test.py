e61b3faffb1f3d12546190b5b713afa74f66ff20
EloteDario <dariox07gm@gmail.com>
2025-08-18 15:48:36 -0500

    day two of github

--- Test.py
+++ Test.py
@@ -1,7 +1,34 @@
-print("""
- EEEEE  L      OOO  TTTTT  EEEEE
- E      L     O   O   T    E
- EEEE   L     O   O   T    EEEE
- E      L     O   O   T    E
- EEEEE  LLLLL  OOO    T    EEEEE
-""")
+import re
+import secrets
+import string
+
+
+def generate_password(length, nums, special_chars, uppercase, lowercase):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        
+        constraints = [
+            (nums, '[0-9]'),
+            (lowercase, '[a-z]'),
+            (uppercase, '[A-Z]'),
+            (special_chars, '')
+        ]        
+
+    return password
+    
+# new_password = generate_password(8)
+# print(new_password)
+pattern = '[^a-z]t'
+quote = 'Not all those who wander are lost.'
+print(re.findall(pattern, quote))
