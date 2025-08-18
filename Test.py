01cf1c6fbc68b2e2661fc0404ec52dd2dfdb7f58
rdlook04 <rdlook04@gmail.com>
2025-08-17 20:27:59 -0500

    Elote Rulex

--- Test.py
+++ Test.py
@@ -1,1 +1,7 @@
-print("hello")
+print("""
+ EEEEE  L      OOO  TTTTT  EEEEE
+ E      L     O   O   T    E
+ EEEE   L     O   O   T    EEEE
+ E      L     O   O   T    E
+ EEEEE  LLLLL  OOO    T    EEEEE
+""")
